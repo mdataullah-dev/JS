e45f2774c5c90f227c123360f69de35fc0995148
Md Ataullah Masood <info.mdataullah@gmail.com>
2026-02-09 18:01:06 +0530

    math

---
+++ work.js
@@ -0,0 +1,37 @@
+// this is for number 
+// this is for maths
+
+// const num = new Number(500)
+// console.log(num);   //? object bana rahe hum aise 
+
+// console.log(num.toString().length);  //? number ko string me convert karne ke liye toString method ka use karte hai
+// console.log(num.toFixed(2));  //? number ko string me convert karne ke liye toFixed method ka use karte hai, isme hum decimal point ke baad kitne number chahiye wo specify karte hai
+
+// //! precision 
+
+// const num2 = 123.8567899
+// console.log(num2.toPrecision(3));
+
+// let n = 100000000000 //? difficult to read
+// console.log(n.toExponential());  //? number ko exponential form me convert karne ke liye toExponential method ka use karte hai
+// console.log(n.toLocaleString());  //? isse us standard mein dikhega 
+// console.log(n.toLocaleString('en-IN'))  //? isse Indian standard mein dikhega
+
+
+
+console.log("------------------------MATHS--------------------------");
+
+console.log(Math);
+// console.log(Math.abs(-6));  //? absolute value
+// console.table([Math.round(4.6), Math.round(4.3)]);  //? round off the number
+// console.table([Math.ceil(4.3), Math.ceil(4.7)]);  //? isme bada hi karega up the number
+// console.table([Math.floor(4.7), Math.floor(4.3)]); //? isme chota hi karega down the number
+
+console.log(Math.random()); //? 0 se 1 ke beech me random number generate karta hai
+
+
+
+
+
+
+
