11db659a4811698a0427b06923be5615980e1886
Md Ataullah Masood <info.mdataullah@gmail.com>
2026-02-10 15:41:53 +0530

    rest operator

--- func.js
+++ func.js
@@ -9,14 +9,25 @@
 // console.log(loginUserMessage())   //? gives //Please Enter a Username
                                   //? undefined just logged in
 
-function loginUserMessage(username = "John"){   //* aisa krne se username hamesha ke liye john fixed ho gya ab user kuch de na de use john ayegag
-    if(!username){
-        console.log("Please Enter a Username");
-        return   
-    }
-    return `${username} just logged in`
+//! function loginUserMessage(username = "John"){   //* aisa krne se username hamesha ke liye john fixed ho gya ab user kuch de na de use john ayegag
+// !    if(!username){
+// !        console.log("Please Enter a Username");
+// !        return   
+// !    }
+// !    return `${username} just logged in`
+//! }
+// !console.log(loginUserMessage("Nazish"))   
+
+//********************************************************************/
+
+//? E-commerce website 
+
+function calculateall(...allnums){  //? rest opertor which will bind all elements in array form
+    return allnums
 }
-console.log(loginUserMessage("Nazish"))   
+console.log(calculateall(200,300,400))
+
+
 
 
 
