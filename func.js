e2edc3ee1119c9104a045673542b578fe1c3a1b9
Md Ataullah Masood <info.mdataullah@gmail.com>
2026-02-10 17:27:50 +0530

    mini hoisting

--- func.js
+++ func.js
@@ -22,10 +22,37 @@
 
 //? E-commerce website 
 
-function calculateall(...allnums){  //? rest opertor which will bind all elements in array form
-    return allnums
+// function calculateall(...allnums){  //? rest opertor which will bind all elements in array form
+//     return allnums
+// }
+// console.log(calculateall(200,300,400))
+
+//????????????????????????
+
+
+
+//! SCOPING
+
+//? {} = scope 
+//* scope level //? mini hoisting
+
+
+//!hoisting 
+console.log(addone(5))    // function ke declartion se pehle v hum usko acces kr paa rahe due to hoisting
+function addone(num){    //? function ko likhne ka treeka 
+    return num + 1;
 }
-console.log(calculateall(200,300,400))
+//console.log(addone(5))
+
+
+
+//!=============================
+//addtwo(10)   //? ye error dega => cause declare baad mein hai 
+const addtwo = function(num){   //? function ko aise v bana skte hain kisi varaible mein daal kr 
+    return num + 2;
+}
+console.log(addtwo(10))  // yahan pr agar is tarah se function ko varaible ke ander declare karengein toh uske declaration ke baad hi hum kr skte acces ya call us function ko
+
 
 
 
