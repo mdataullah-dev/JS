b99b0972788265bd02d701685cd2cc5e6d60ee83
Md Ataullah Masood <info.mdataullah@gmail.com>
2026-02-11 12:37:14 +0530

    iterators

---
+++ core.js
@@ -0,0 +1,64 @@
+  //? LOOPS 
+
+
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    //console.log(element);
+    
+    
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    if(i==5){
+        //console.log(`EXIT ${i} Detected`);
+        //break;
+        //?console.log(`skipping ${i}`);
+        //?continue;
+        
+    }
+    //console.log(i);
+}
+
+//? while and do-while loops 
+let heros = new Array(1,2,4,4,5)
+let i = 0
+while(i<=heros.length){
+    //?console.log(`the element is ${heros[i]}`);
+    i++;
+    
+}   //? last mein undefined aa gya coz humne wahan pr <= kr diya is wajah se undefined aaya
+
+
+//? do-while
+
+let score = 100;
+do{
+    //?console.log(`score is ${score}`);
+    score++;
+    
+}while(score<=110)
+
+//!=================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
