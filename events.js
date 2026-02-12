626aeba27d7fb6f12f067fc22cb38bf551ae9101
Md Ataullah Masood <info.mdataullah@gmail.com>
2026-02-12 09:52:02 +0530

    events

---
+++ events.js
@@ -0,0 +1,40 @@
+//? events in javascript is imp
+
+//?  in js all the events runs sequencially 
+
+// ! events && events handling  
+
+//? how we do work 
+
+
+//event //?  =>  clicked krna .. ye ek event hai 
+
+// const a = ()  => 7 + 9 
+// console.log(a());
+
+//? output we get that is 16
+
+// now we will move forward to events but 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
